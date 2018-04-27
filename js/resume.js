f62f627cea05213c870c09648025ea127ca1a8e8
liuqqcode <1761112388@qq.com>
2018-04-27 09:54:49 +0800

    .....

--- js/resume.js
+++ js/resume.js
@@ -2,13 +2,13 @@ var  xsd = $("xiaoshoudiv");
 
 var workListShow = function () {
     if ($("#xiaoshouid")[0].style.display == 'none'){
-        $("#xiaoshouid").show(500);
+        $("#xiaoshouid").slideDown(500  );
 
 
     }
 }
 
 var workListClose = function () {
-        $("#xiaoshouid").hide(100);
+        $("#xiaoshouid").slideUp(500);
 
 }
