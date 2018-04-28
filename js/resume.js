500a05cd1c0ba6ee00e3448be2e64d66252559ab
liuqwertyuiop <1761112388@qq.com>
2018-04-28 09:12:27 +0800

    修改搜我又是那个是

--- js/resume.js
+++ js/resume.js
@@ -1,14 +1,18 @@
-var  xsd = $("xiaoshoudiv");
-
+// 销售主管弹窗开关
 var workListShow = function () {
     if ($("#xiaoshouid")[0].style.display == 'none'){
         $("#xiaoshouid").slideDown(500  );
-
-
+        }
     }
-}
-
 var workListClose = function () {
         $("#xiaoshouid").slideUp(500);
+    }
+$(document).ready(function(){
+    $("body").mousedown(function(){
+            $("#xiaoshouid").slideUp(500);
+        })
+    $("")
 
-}
+})
+
+// 背景canvas
